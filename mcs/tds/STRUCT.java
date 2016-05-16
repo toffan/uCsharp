31eb4b187c47165b77ccaf14d529192b3efeaa02
toffan <benoit.lemarchand@inpt.fr>
2016-05-16 14:44:01 +0200

    Ajoute searchVar et putVar à STRUCT

--- mcs/tds/STRUCT.java
+++ mcs/tds/STRUCT.java
@@ -7,5 +7,8 @@ public class STRUCT extends TYPE {
 
     ///   Constructeurs   ///
     public STRUCT() { super(null, 0); };
-}
 
+    ///   Méthodes   ///
+    public VAR searchVar(String id) { return this.tds.searchVar(id, true); }
+    public void putVar(String id, TYPE type) { this.tds.putVar(id, type); }
+}
