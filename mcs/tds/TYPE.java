fbab2c86f5b4fb4c1bfbe2862f90d2b1774a8990
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-05-18 20:54:12 +0200

    Work with egg and instanceof.

--- mcs/tds/TYPE.java
+++ mcs/tds/TYPE.java
@@ -13,6 +13,37 @@ public class TYPE {
         this.size = size;
     }
 
+    ///   Proxy InstanceOf   ///
+    boolean isStruct()
+    {
+        return (this instanceof STRUCT);
+    }
+
+    boolean isPointer()
+    {
+        return (this instanceof PTR);
+    }
+
+    boolean isFType()
+    {
+        return (this instanceof FTYPE);
+    }
+
+    STRUCT toStruct()
+    {
+        return (STRUCT) this;
+    }
+
+    PTR toPointer()
+    {
+        return (PTR) this;
+    }
+
+    FTYPE toFType()
+    {
+        return (FTYPE) this;
+    }
+
     ///   Methodes   ///
     public int size() { return this.size; }
     public String name() { return this.name; }
