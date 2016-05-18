3cbde9934c5fc7bbebfa79187523195585de02d7
stoakes <antoine.beyet@gmail.com>
2016-05-18 17:23:54 +0200

    Interface et implémentation du boolHelper

--- mcs/lib/general/HelperBoolInterface.java
+++ mcs/lib/general/HelperBoolInterface.java
@@ -1,18 +1,62 @@
 package general;
 
 public interface HelperBoolInterface {
-	/**
-	 * A rediscuter de comment on l'implemente
-	 * 
-	 * negation logique (une adresse)
-	 * negation logique	(une valeur)
-	 * 
-	 * ou logique (un adresse, une valeur)
-	 * ou logique	(2 valeurs)
-	 * ou logique	(2 adresses)
-	 * 
-	 * et logique (un adresse, une valeur)
-	 * et logique	(2 valeurs)
-	 * et logique	(2 adresses)
-	 */
+
+    /**
+     * Negation d'une valeur
+     * @param value
+     * @return
+     */
+    public String generateNegation(String value);
+
+    /**
+     * Negation d'une valeur
+     * @param dep
+     * @param taille
+     * @return
+     */
+    public String generateNegation(int dep, int taille);
+
+    /**
+     * or de 2 booléens (adresse,valeur)
+     * @param dep
+     * @param size
+     * @param value
+     * @return
+     */
+    public String generateOr(int dep, int size, String value);
+
+    /** or entre 2 valeurs **/
+    public String generateOr(String value1, String value2);
+
+    /** or entre 2 adresses **/
+    public String generateOr(int dep1, int size1, int dep2, int size2);
+
+    /** and entre 1 adresse une valeur **/
+    public String generateAnd(int dep, int size, String value);
+
+    /** and entre 2 valeurs **/
+    public String generateAnd(String value1, String value2);
+
+    /** and entre 2 adresses **/
+    public String generateAnd(int dep1, int size1, int dep2, int size2);
+
+    /** affiche une valeur bool sur stdout **/
+    public String generateDisplayBool(String value);
+
+    /** affiçche une adresse bool sur stdxout **/
+    public String generateDisplayBool(int dep, int size);
+
+    /** input d'un bool sur stdin **/
+    public String generateReadBool();
+
+    /** bool to char **/
+    public String generateBoolToChar();
+
+    /** bool to int **/
+    public String generateBoolToInt();
+
+    /** bool to string **/
+    public String generateBoolToString();
+
 }
