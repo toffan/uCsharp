ba502f7bef6fa6fc072ef018993167eb539336df
Volodia Laniel <laniel.volodia@gmail.com>
2016-05-19 18:55:43 +0200

    Séparation librairies MCS et ASM dans paquetages séparés.

---
+++ mcs/libasm/tds/INFO.java
@@ -0,0 +1,14 @@
+//--------------------------------------------------
+// INFO la classe representant une info de ASMTDS
+//--------------------------------------------------
+package mcs.libasm.tds;
+
+/**
+ * Element d'une ASMTDS
+ * 
+ * @author marcel
+ * 
+ */
+public interface INFO {
+
+}
