fd608f1b38c58c2855f12a0119f8816c70cc0aa1
Volodia Laniel <laniel.volodia@gmail.com>
2016-05-29 14:53:25 +0200

    Modification du fonctionnement de l'API gc : opérations sur les entiers.

--- mcs/gc/general/HelperIntInterface.java
+++ mcs/gc/general/HelperIntInterface.java
@@ -2,76 +2,73 @@ package mcs.gc.general;
 
 public interface HelperIntInterface {
 
-    /** sommer 2 entiers (un adresse (sur la pile), une valeur) **/
-    public String generateSum(int dep, int size, String value);
+    /** sommer 2 entiers precedemment charges sur la pile. */
+    public String generateSum();
 
-    /** sommer 2 entiers (2 valeurs) **/
-    public String generateSum(String value1, String value2);
+    /** soustraire 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) - (dernier entier pousse sur la pile).
+     * */
+    public String generateSub();
 
-    /** sommer 2 entiers (2 adresses) **/
-    public String generateSum(int dep1, int size1, int dep2, int size2);
+    /** diviser 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) / (dernier entier pousse sur la pile).
+     * */
+    public String generateDiv();
 
-    /** soustraire 2 entiers (un adresse, une valeur) **/
-    public String generateSub(int dep, int size, String value);
+    /** multiplier 2 entiers precedemment charges sur la pile. */
+    public String generateMul();
 
-    /** soustraire 2 entiers (une valeur, une adresse) - pcq sub ce n'est pas
-     * commutatif !! **/
-    public String generateSub(String value, int dep, int size);
+    /** modulo 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) % (dernier entier pousse sur la pile).
+     * */
+    public String generateMod();
 
-    /** soustraire 2 entiers	(2 valeurs) **/
-    public String generateSub(String value1, String value2);
+    /** comparer (egalite) 2 entiers precedemment charges sur la pile.
+     * (Premier entier) == (dernier entier pousse sur la pile).
+     * */
+    public String generateEq();
 
-    /** soustraire 2 entiers	(2 adresses) **/
-    public String generateSub(int dep1, int size1, int dep2, int size2);
+    /** comparer (inegalite) 2 entiers precedemment charges sur la pile.
+     * (Premier entier) != (dernier entier pousse sur la pile).
+     * */
+    public String generateNeq();
 
-    /** diviser 2 entiers (un adresse, une valeur) **/
-    public String generateDiv(int dep, int size, String value);
+    /** comparer (inferieur strict) 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) < (dernier entier pousse sur la pile).
+     * */
+    public String generateLss();
 
-    /** diviser 2 entiers (une valeur, une adresse) **/
-    public String generateDiv(String value, int dep, int size);
+    /** comparer (inferieur ou egal) 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) <= (dernier entier pousse sur la pile).
+     * */
+    public String generateLeq();
 
-    /** diviser 2 entiers	(2 valeurs) **/
-    public String generateDiv(String value1, String value2);
+    /** comparer (superieur strict) 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) > (dernier entier pousse sur la pile).
+     * */
+    public String generateGtr();
 
-    /** diviser 2 entiers	(2 adresses) **/
-    public String generateDiv(int dep1, int size1, int dep2, int size2);
+    /** comparer (superieur ou egal) 2 entiers precedemment charges sur la pile.
+     * A verifier :
+     * (Premier entier) >= (dernier entier pousse sur la pile).
+     * */
+    public String generateGeq();
 
-    /** multiplier 2 entiers (un adresse, une valeur) **/
-    public String generateMul(int dep, int size, String value);
+    /** Conversion d'un entier (precedemment charge sur la pile) en booleen. */
+    public String generateIntToBool();
 
-    /** multiplier 2 entiers	(2 valeurs) **/
-    public String generateMul(String value1, String value2);
-
-    /** multiplier 2 entiers	(2 adresses) **/
-    public String generateMul(int dep1, int size1, int dep2, int size2);
-
-    /** comparer egalite 2 entiers (un adresse, une valeur) **/
-    public String generateComEqu(int dep, int size, String value);
-
-    /** comparer egalite 2 entiers	(2 valeurs) **/
-    public String generateComEqu(String value1, String value2);
-
-    /** comparer egalite 2 entiers	(2 adresses) **/
-    public String generateComEqu(int dep1, int size1, int dep2, int size2);
-
-    /** modulo 2 entiers (un adresse, une valeur) **/
-    public String generateMod(int dep, int size, String value);
-
-    /** modulo 2 entiers (une valeur, un adresse) **/
-    public String generateMod(String value, int dep, int size);
-
-    /** modulo 2 entiers (2 valeurs) **/
-    public String generateMod(String value1, String value2);
-
-    /** modulo 2 entiers (2 adresses) **/
-    public String generateMod(int dep1, int size1, int dep2, int size2);
-
-    /** afficher un entier sur stdout (valeur) **/
+    /** afficher un entier sur stdout (valeur) */
     public String generateDisplay(String value);
 
-    /** afficher un entier sur stdout (adresse) **/
+    /** afficher un entier sur stdout (adresse) */
     public String generateDisplay(int dep, int size);
 
-    /**lire un entier sur stdin **/
+    /**lire un entier sur stdin */
     public String generateRead();
 }
--- mcs/gc/tam/TamHelperInt.java
+++ mcs/gc/tam/TamHelperInt.java
@@ -9,184 +9,76 @@ import mcs.gc.general.*;
  */
 public class TamHelperInt implements HelperIntInterface {
 
-
     @Override
-    public String generateSum(int dep, int size, String value) {
-        return "; somme (" + dep + "," + size + ") + " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateSum() {
+        return "; somme des deux entiers charges precedemment.\n"
             + "\tSUBR IAdd\n";
     }
 
     @Override
-    public String generateSum(String value1, String value2) {
-        return "; somme " + value1 + " + " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR IAdd";
-    }
-
-    @Override
-    public String generateSum(int dep1, int size1, int dep2, int size2) {
-        return "; somme (" + dep1 + "," + size1 + ") + (" + dep2 + "," + size2 +
-            ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR IAdd\n";
-    }
-
-    @Override
-    public String generateSub(int dep, int size, String value) {
-        return "; soustraction (" + dep + "," + size + ") - " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateSub() {
+        return "; soustraction des deux entiers charges precedemment (le dernier entier charge etant celui a soustraire).\n"
             + "\tSUBR ISub\n";
     }
 
     @Override
-    public String generateSub(String value, int dep, int size) {
-        return "; soustraction " + value + " - (" + dep + "," + size + ") \n"
-            + "\tLOADL " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tSUBR ISub\n";
-    }
-
-    @Override
-    public String generateSub(String value1, String value2) {
-        return "; soustraction " + value1 + " + " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR ISub\n";
-    }
-
-    @Override
-    public String generateSub(int dep1, int size1, int dep2, int size2) {
-        return "; soustraction (" + dep1 + "," + size1 + ") + (" + dep2 + "," +
-            size2 + ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR ISub\n";
-    }
-
-    @Override
-    public String generateDiv(int dep, int size, String value) {
-        return "; division entiere (" + dep + "," + size + ") par " + value +
-            "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateDiv() {
+        return "; division de deux entiers charges precedemment (le dernier entier charge etant le denominateur).\n"
             + "\tSUBR IDiv\n";
     }
 
     @Override
-    public String generateDiv(String value, int dep, int size) {
-        return "; division entiere  " + value + " par (" + dep + "," + size +
-            ")\n"
-            + "\tLOADL " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tSUBR IDiv\n";
-    }
-
-    @Override
-    public String generateDiv(String value1, String value2) {
-        return "; division entiere " + value1 + " par " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR IDiv\n";
-    }
-
-    @Override
-    public String generateDiv(int dep1, int size1, int dep2, int size2) {
-        return "; division entiere (" + dep1 + "," + size1 + ") par (" + dep2 +
-            "," + size2 + ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR IDiv\n";
-    }
-
-    @Override
-    public String generateMul(int dep, int size, String value) {
-        return "; multiplication de (" + dep + "," + size + ") par " + value +
-            "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateMul() {
+        return "; multiplication de deux entiers charges precedemment.\n"
             + "\tSUBR IMul";
     }
 
     @Override
-    public String generateMul(String value1, String value2) {
-        return "; multiplication de " + value1 + " par " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR IMul\n";
-    }
-
-    @Override
-    public String generateMul(int dep1, int size1, int dep2, int size2) {
-        return "; multiplication de  (\"+dep1+\",\"+size1+\")par (" + dep2 +
-            "," + size2 + ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR IMul\n";
-    }
-
-    @Override
-    public String generateMod(int dep, int size, String value) {
-        return "; modulo de (" + dep + "," + size + ") par " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateMod() {
+        return "; modulo de deux entiers charges precedemment.\n"
             + "\tSUBR IMod\n";
     }
 
     @Override
-    public String generateMod(String value, int dep, int size) {
-        return "; modulo de " + value + " par (" + dep + "," + size + ") \n"
-            + "\tLOADL " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tSUBR IMod\n";
-    }
-
-    @Override
-    public String generateMod(String value1, String value2) {
-        return "; modulo de " + value1 + " par " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR IMod\n";
-    }
-
-    @Override
-    public String generateMod(int dep1, int size1, int dep2, int size2) {
-        return "; modulo de (" + dep1 + "," + size1 + ") par (" + dep2 + "," +
-            size2 + ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR IMod\n";
-    }
-
-    @Override
-    public String generateComEqu(int dep, int size, String value) {
-        return "; comparaison egalite (" + dep + "," + size + ") et " + value +
-            "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
-            + "\tSUBR IEq\n";
-    }
-
-
-    @Override
-    public String generateComEqu(String value1, String value2) {
-        return "; comparaison egalite " + value1 + " et " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
+    public String generateEq() {
+        return "; comparaison (egalite) entre deux entiers charges precedemment.\n"
             + "\tSUBR IEq\n";
     }
 
     @Override
-    public String generateComEqu(int dep1, int size1, int dep2, int size2) {
-        return "; comparaison egalite (" + dep1 + "," + size1 + ") et (" +
-            dep2 + "," + size2 + ")\n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR Ieq\n";
+    public String generateNeq() {
+        return "; comparaison (inegalite) entre deux entiers charges precedemment.\n"
+            + "\tSUBR INeq\n";
+    }
+
+    @Override
+    public String generateLss() {
+        return "; comparaison (inferieur strict) entre deux entiers charges precedemment.\n"
+            + "\tSUBR ILss\n";
+    }
+
+    @Override
+    public String generateLeq() {
+        return "; comparaison (inferieur ou egal) entre deux entiers charges precedemment.\n"
+            + "\tSUBR ILeq\n";
+    }
+
+    @Override
+    public String generateGtr() {
+        return "; comparaison (superieur strict) entre deux entiers charges precedemment.\n"
+            + "\tSUBR IGtr\n";
+    }
+
+    @Override
+    public String generateGeq() {
+        return "; comparaison (superieur ou egal) entre deux entiers charges precedemment.\n"
+            + "\tSUBR IGeq\n";
+    }
+
+    @Override
+    public String generateIntToBool() {
+        return "; conversion d'un entier precedemment charge en booleen.\n"
+            + "\tSUBR I2B\n";
     }
 
     @Override
@@ -202,7 +94,6 @@ public class TamHelperInt implements HelperIntInterface {
             + "\tLOAD(" + size + ") " + dep + "[LB] \n"
             + "\tSUBR IOut\n";
     }
-
 
     @Override
     public String generateRead() {
