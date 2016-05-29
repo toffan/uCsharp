98129bfd9fe800f05f52a6f12a5e46e18368e8e7
Volodia Laniel <laniel.volodia@gmail.com>
2016-05-29 16:04:42 +0200

    Modification du fonctionnement de l'API gc : opérations sur les booleens.

--- mcs/gc/tam/TamHelperBool.java
+++ mcs/gc/tam/TamHelperBool.java
@@ -8,64 +8,20 @@ import mcs.gc.general.*;
  */
 public class TamHelperBool implements HelperBoolInterface {
 
-    public String generateNegation(String value) {
-        return "; negation bool"
-            + "\tLOADL " + value + "\n"
+    public String generateNegation() {
+        return "; negation bool\n"
             + "\tSUBR BNeg";
     }
 
-    public String generateNegation(int dep, int taille) {
-        return "; negation bool"
-            + "\tLOAD(" + taille + ") " + dep + "[LB] \n"
-            + "\tSUBR BNeg";
-    }
-
-    public String generateOr(int dep, int size, String value) {
-        return "; or (" + dep + "," + size + ") + " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateOr() {
+        return "; disjonction de deux booleens precedemment charges sur la pile.\n"
             + "\tSUBR BOr\n";
     }
 
-
-    public String generateOr(String value1, String value2) {
-        return "; or " + value1 + " et " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR BOr\n";
-    }
-
-    public String generateOr(int dep1, int size1, int dep2, int size2) {
-        return "; or (" + dep1 + "," + size1 + ") + (" + dep2 + "," + size2 +
-            ") \n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR BOr\n";
-    }
-
-    public String generateAnd(int dep, int size, String value) {
-        return "; and (" + dep + "," + size + ") + " + value + "\n"
-            + "\tLOAD(" + size + ") " + dep + "[LB] \n"
-            + "\tLOADL " + value + "\n"
+    public String generateAnd() {
+        return "; conjonction de deux booleens precedemment charges sur la pile.\n"
             + "\tSUBR BAnd\n";
     }
-
-
-    public String generateAnd(String value1, String value2) {
-        return "; and " + value1 + " et " + value2 + "\n"
-            + "\tLOADL " + value1 + "\n"
-            + "\tLOADL " + value2 + "\n"
-            + "\tSUBR BAnd\n";
-    }
-
-    public String generateAnd(int dep1, int size1, int dep2, int size2) {
-        return "; and (" + dep1 + "," + size1 + ") + (" + dep2 + "," + size2 +
-            ") \n"
-            + "\tLOAD(" + size1 + ") " + dep1 + "[LB] \n"
-            + "\tLOAD(" + size2 + ") " + dep2 + "[LB] \n"
-            + "\tSUBR BAnd\n";
-    }
-
 
     public String generateDisplay(String value) {
         return "; display Bool " + value + "\n"
